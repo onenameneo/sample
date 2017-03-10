e3e92d65f6e64f2837438354eabe23c8aabfaf7e
vagrant <vagrant@homestead>
2017-03-10 07:55:22 +0000

    Finish user sigup

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,20 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+
+    /**
+     * 利用gravatar创建头像
+     *
+     * @param string $size
+     * @return string
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+
+
 }
